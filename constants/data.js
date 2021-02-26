8315bb2f6621b74239af33a81d81bb0e514529ef
Chepman32 <53965045+Chepman32@users.noreply.github.com>
2021-02-26 19:01:22 +0300

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -99,4 +99,125 @@ export const data = [
     answer: 27,
     img: require("../assets/img/27.jpg"),
   },
+  
+  {
+    question: "How many micrograms of caffeine in an athlete's blood is enough to be disqualified in the Olympics Games?",
+    answer: 12,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many thousands of chocolates were in the world's largest candy box?",
+    answer: 90,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many centimeters per year does the San Andreas Fault advance, bringing Los Angeles closer to San Francisco?",
+    answer: 5,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many years in prison can one get in Thailand for criticizing the royal family?",
+    answer: 15,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "What percentage of all mammals on Earth are bats?",
+    answer: 20,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many days is the life cycle of a sperm?",
+    answer: 75,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many LEGO parts are there for each inhabitant of the planet?",
+    answer: 30,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many liters of fruit juice does each Canadian citizen drink each week?",
+    answer: 1,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many percent of the world's cultural heritage is in Italy?",
+    answer: 60,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many kilograms does the skeleton of an average person weigh?",
+    answer: 13,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many centimeters is the length of the giraffe's tongue?",
+    answer: 45,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many percent of the air in is the volume of each package of chips `Lays`?",
+    answer: 41,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "What is the percentage of yellow candy in each candy pack `Skittles`?",
+    answer: 26,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many months of their lives do residents of large cities spend waiting for the green light of a traffic light?",
+    answer: 6,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many times has the Statue of Liberty been repainted in a new color?",
+    answer: 13,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How old is the average Russian citizen?",
+    answer: 40,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How much did it cost to produce the lowest-budget feature-length film in history?",
+    answer: 70,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many monkeys have been in space?",
+    answer: 32,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many thousand square meters is the area of the largest QR code in the world?",
+    answer: 29,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many percent of women choose to correct their bad mood with fast food?",
+    answer: 26,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many percent of women prefer to dine in a restaurant or cafe, or not to dine at all?",
+    answer: 25,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many percent of women do not want to take their husband's surname?",
+    answer: 51,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many percent of Russians read the operating instructions for new household appliances?",
+    answer: 3,
+    img: require("../assets/img/27.jpg"),
+  },
+  {
+    question: "How many weeks is paid vacation in Italy?",
+    answer: 9,
+    img: require("../assets/img/27.jpg"),
+  },
 ]
